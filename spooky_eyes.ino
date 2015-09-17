ccce1153c458bc39d85f937eccddaa47a5fa1735
Pim Voeten <pim.voeten@gmail.com>
2015-09-17 12:37:53 +0200

    Refactored eyes. Easier to create scenes. Scenes are weighted randomly started. More dramatic opening and closing of the eyes

--- spooky_eyes.ino
+++ spooky_eyes.ino
@@ -1,78 +1,90 @@
 const int leftEyePin = 9;
 const int rightEyePin = 10;
 
-int currentEffect = 0;
-long startEffect = 0;
+long startTimeEffect = 0;
 
-const int minEffectTime = 30000; // Minimum duration of effect
-const int maxEffectTime = 60000; // Maximum duration of effect
-const int effects = 3; // amount of active effects;
+const int minEffectTime = 1 * 60000; // Minimum duration of effect
+const int maxEffectTime = 5 * 60000; // Maximum duration of effect
+
+// Eyes status
+const String OPEN = "OPEN";
+const String CLOSED = "CLOSED";
+const String FLICKER = "FLICKER";
+String status = CLOSED;
 
 // Blinking
-long blinkTime = 0;
-int nextBlinkTime = 0;
 const int minBlinkTime = 10000; // Blink not before
 const int maxBlinkTime = 15000; // Blink before
+long lastBlinkTime = millis();
+long nextBlinkTime = random(minBlinkTime, maxBlinkTime);
 
 // Flickering
-const int maxFlickeringTime = 10000; // Maximum ms flickering may occur
+const int MAX_FLICK_TIME = 20000; // Maximum ms flickering may occur
+
+// Weighted effects (sum must be 100)
+// _NR must correspond to the index in the array!!!
+const int STARING_WEIGHT = 50;
+const int STARING_NR = 0;
+
+const int SLEEPING_WEIGHT = 35;
+const int SLEEPING_NR = 1;
+
+const int FLICKERING_WEIGHT = 15;
+const int FLICKERING_NR = 2;
+
+const int EFFECTS[] = {STARING_WEIGHT, SLEEPING_WEIGHT, FLICKERING_WEIGHT};
+const int EFFECT_COUNT = 3; // length of array
+int activeEffect;
 
 void setup() {
   Serial.begin(115200);
-  openEyes(30, 5, true);
-  nextBlinkTime = random(minBlinkTime, maxBlinkTime);
+  randomSeed(analogRead(0));
+//  delay(5000);
+  stare();
 }
 
 void loop() {
-  if (currentEffect == 1 && (millis() - blinkTime) >= nextBlinkTime) {
-    doBlink();
+  if (activeEffect == STARING_NR && (millis() - lastBlinkTime) >= nextBlinkTime) {
+    doBlink(1);
     nextBlinkTime = random(minBlinkTime, maxBlinkTime);
   }
 
-  if (currentEffect == 2) {
-    if (millis() - startEffect <= maxFlickeringTime) {
-      flicker(false);
-    } else {
-      // Stop flickering
-      openEyes(30, 5, true);
-    }
-  }
-
   // wait some time before a new effect can occur
-  if (millis() - startEffect > random(minEffectTime, maxEffectTime)) {
-    int effect = random(0, effects);
-    if (effect != currentEffect) {
-      Serial.println("Old effect: " + (String)currentEffect + " New effect: " + (String)effect);
-      switch (effect) {
-        case 0: closeEyes(30, 5, true); break;
-        case 1: openEyes(30, 5, true); break;
-        case 2: flicker(true); break;
-      }
-      currentEffect = effect;
+  if (millis() - startTimeEffect > random(minEffectTime, maxEffectTime)) {
+    int newEffect = getEffect();
+    startTimeEffect = millis();
+    switch (newEffect) {
+      case STARING_NR: stare(); break;
+      case SLEEPING_NR: sleep(); break;
+      case FLICKERING_NR: flicker(MAX_FLICK_TIME); break;
     }
   }
 }
 
-void closeEyes(int ms, int steps, boolean start) {
-  if (start) {
-    Serial.println("Close eyes");
-    startEffect = millis();
+int getEffect() {
+  int effect = random(0, 100);
+  Serial.println("New random: " + (String)effect);
+  int x = 0;
+  for (int i = 0; i < EFFECT_COUNT; i++) {
+    x += EFFECTS[i];
+    if (x >= effect) {
+      return i;
+    }
   }
-  currentEffect = 0;
+}
+
+void closeEyes(int ms, int steps) {
+  Serial.println("Closing eyes");
   for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= steps) {
     analogWrite(leftEyePin, fadeValue);
     analogWrite(rightEyePin, fadeValue);
     delay(ms);
   }
+  status = CLOSED;
 }
 
-void openEyes(int ms, int steps, boolean start) {
-  if (start) {
-    Serial.println("Open eyes");
-    startEffect = millis();
-    blinkTime = 0;
-  }
-  currentEffect = 1;
+void openEyes(int ms, int steps) {
+  Serial.println("Opening eyes");
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += steps) {
     analogWrite(leftEyePin, fadeValue);
     analogWrite(rightEyePin, fadeValue);
@@ -80,29 +92,78 @@ void openEyes(int ms, int steps, boolean start) {
   }
   analogWrite(leftEyePin, 255);
   analogWrite(rightEyePin, 255);
+  status = OPEN;
 }
 
-void flicker(boolean start) {
-  if (start) {
-    Serial.println("Flicker eyes");
-    startEffect = millis();
-    currentEffect = 2;
+/*
+   STARING: effect
+
+   Eyes are open and blink once in a while
+*/
+void stare() {
+  if (status == CLOSED) {
+    // Wakeup dramatically
+    for (int i = 0; i < 5; i++) {
+      openEyes(3*i, 50);
+      delay(30*i);
+      closeEyes(3*i, 50);
+      delay(30*i);
+    }
+    lastBlinkTime = millis();
+    openEyes(30, 5);
   }
-  if (currentEffect == 0) {
-    openEyes(30, 5, false);
-  }
-  int value = random(0, 200);
-  analogWrite(leftEyePin, value);
-  analogWrite(rightEyePin, value);
-  delay(50);
+  activeEffect = STARING_NR;
+  Serial.println("-- EFFECT: Staring");
 }
 
-// Part of openEyes. Blink every 2 seconds
-void doBlink() {
-  Serial.println("Blink after " + (String)(millis() - blinkTime) + " ms");
-  blinkTime = millis();
-  if (currentEffect == 1) {
-    closeEyes(5, 20, false);
-    openEyes(5, 20, false);
+/*
+   SLEEPING: effect
+
+   Eyes are closed
+*/
+void sleep() {
+  if (status == OPEN) {
+    closeEyes(30, 5);
+  }
+  activeEffect = SLEEPING_NR;
+  Serial.println("-- EFFECT: Sleeping");
+}
+
+/*
+   FLICKERING: effect
+
+   Eyes burst into berserk flaming
+*/
+void flicker(long duration) {
+  if (status = CLOSED) {
+    openEyes(10, 15);
+  }
+  activeEffect = FLICKERING_NR;
+  Serial.println("-- EFFECT: Flickering");
+  while (millis() - startTimeEffect <= duration) {
+    int value = 0;
+    if (millis() % 2 == 0) {
+      value = random(0, 100);
+    } else {
+      value = random(155, 255);
+    }
+    analogWrite(leftEyePin, value);
+    analogWrite(rightEyePin, value);
+    delay(100);
+  }
+  doBlink(3);
+  delay(250);
+  sleep();
+}
+
+void doBlink(int times) {
+  Serial.println("Blinking after " + (String)(millis() - lastBlinkTime) + " ms");
+  lastBlinkTime = millis();
+  for (int i = 0; i < times; i++) {
+    if (status == OPEN) {
+      closeEyes(10, 15);
+      delay(500);
+      openEyes(10, 15);
+    }
   }
 }
